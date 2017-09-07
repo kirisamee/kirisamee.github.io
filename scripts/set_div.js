99613b24dc32bb3297a9a511afcb19f05aa67af7
wangyongxuan <wangyongxuan@wangyongxuandeMacBook-Pro.local>
2017-09-07 22:39:36 +0800

    New

--- scripts/set_div.js
+++ scripts/set_div.js
@@ -1,24 +1,24 @@
-var log = console.log.bind(console);
-var createDiv = function(){
-
-    var descDiv = document.createElement('div');
-    document.body.appendChild(descDiv);
-    var canvas = document.getElementById('puzzle');
-    log(canvas);
-    var context = canvas.getContext('2d');
-    var divWidth = canvas.offsetWidth;
-    var divHeight = canvas.offsetHeight;
-    var cssStr = "z-index:5;width:"+divWidth+"px;height:"+divHeight+"px;"+
-    "background-color:#f99655;position:absolute;display:block";
-
-    descDiv.style.cssText = cssStr;
-    descDiv.innerHTML = '按下start按键开始游戏';
-    descDiv.id = 'descDiv';
-    descDiv.style.filter= "alpha(opacity=50)";
-    descDiv.style.opacity="0.3";
-    descDiv.align="center";
-    descDiv.style.lineHeight = 600 + "px";
-
-}
-
+// var log = console.log.bind(console);
+// var createDiv = function(){
+//
+//     var descDiv = document.createElement('div');
+//     document.body.appendChild(descDiv);
+//     var canvas = document.getElementById('puzzle');
+//     log(canvas);
+//     var context = canvas.getContext('2d');
+//     var divWidth = canvas.offsetWidth;
+//     var divHeight = canvas.offsetHeight;
+//     var cssStr = "z-index:5;width:"+divWidth+"px;height:"+divHeight+"px;"+
+//     "background-color:#f99655;position:absolute;display:block";
+//
+//     descDiv.style.cssText = cssStr;
+//     descDiv.innerHTML = '按下start按键开始游戏';
+//     descDiv.id = 'descDiv';
+//     descDiv.style.filter= "alpha(opacity=50)";
+//     descDiv.style.opacity="0.3";
+//     descDiv.align="center";
+//     descDiv.style.lineHeight = 600 + "px";
+//
+// }
+// 
 //createDiv();
